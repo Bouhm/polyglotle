54f07fc65ff03d0fd6bc3c88e35b333fa434f5a1
Bouhm <bbouhm@gmail.com>
2026-03-10 02:13:10 -0700

    rename from polyglotl to polyglotle

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -5,12 +5,12 @@ export default function About() {
   return (
     <Container maxW="container.lg" color="white" paddingTop="2rem">
       <Head>
-        <title>Polyglotl - About</title>
+        <title>Polyglotle - About</title>
       </Head>
-      <Heading as='h1' size='2xl' marginBottom="2rem">About Hoyodle</Heading>
+      <Heading as='h1' size='2xl' marginBottom="2rem">About Polyglotle</Heading>
       <Stack spacing={3}>
         <Text>
-          Polyglotl is a project made on a whim based on a friend's inquiry which was word-for-word:
+          Polyglotle is a project made (by a human, not AI) on a whim based on a friend's inquiry which was word-for-word:
         </Text>
         <Text textStyle="italic">
           "Does anyone know of an app that will give you a daily vocabulary word for a selection of chosen languages? Like the same word across all languages"
@@ -20,12 +20,12 @@ export default function About() {
         </Text>
         <Divider />
         <Heading as='h1'>
-          Stretch Goals
+          Stretch Goals (meaning I won't add unless requested)
         </Heading>
         <UnorderedList>
           <ListItem>Support base languages other than English</ListItem>
-          <ListItem>Different difficulties</ListItem>
-          <ListItem>Hints system</ListItem>
+          <ListItem>Support more languages</ListItem>
+          <ListItem>Different difficulties, hints system</ListItem>
           <ListItem>Pronunciation guide</ListItem>
         </UnorderedList>
       </Stack>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Polyglotl - Daily Puzzle</title>
+        <title>Polyglotle - Daily Puzzle</title>
         <meta name="description" content="Daily language puzzles" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
